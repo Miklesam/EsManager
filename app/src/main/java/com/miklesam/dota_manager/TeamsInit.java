c54e79a06faf61c30c8db155f07e673eea90a29e
Miklesami <mikle.samarkin@gmail.com>
2019-06-06 09:34:34 +0300

    all teams

--- app/src/main/java/com/miklesam/dota_manager/TeamsInit.java
+++ app/src/main/java/com/miklesam/dota_manager/TeamsInit.java
@@ -8,15 +8,21 @@ import static com.miklesam.dota_manager.PlayersInit.Afoninje;
 import static com.miklesam.dota_manager.PlayersInit.AfterLife;
 import static com.miklesam.dota_manager.PlayersInit.ArsArt;
 import static com.miklesam.dota_manager.PlayersInit.ArsZeeqq;
+import static com.miklesam.dota_manager.PlayersInit.Artstyle;
 import static com.miklesam.dota_manager.PlayersInit.BLACKARXANGEL;
 import static com.miklesam.dota_manager.PlayersInit.Bignum;
 import static com.miklesam.dota_manager.PlayersInit.Blizzy;
+import static com.miklesam.dota_manager.PlayersInit.BloodAngel;
+import static com.miklesam.dota_manager.PlayersInit.CemaTheSlayer;
+import static com.miklesam.dota_manager.PlayersInit.Ceyler;
 import static com.miklesam.dota_manager.PlayersInit.Cooman;
 import static com.miklesam.dota_manager.PlayersInit.Crystallize;
 import static com.miklesam.dota_manager.PlayersInit.Daxak;
+import static com.miklesam.dota_manager.PlayersInit.Dendi;
 import static com.miklesam.dota_manager.PlayersInit.Dread;
 import static com.miklesam.dota_manager.PlayersInit.Fng;
 import static com.miklesam.dota_manager.PlayersInit.G;
+import static com.miklesam.dota_manager.PlayersInit.GeneRaL;
 import static com.miklesam.dota_manager.PlayersInit.Ghostik;
 import static com.miklesam.dota_manager.PlayersInit.Goblak;
 import static com.miklesam.dota_manager.PlayersInit.Iceberg;
@@ -24,13 +30,17 @@ import static com.miklesam.dota_manager.PlayersInit.Immersion;
 import static com.miklesam.dota_manager.PlayersInit.Jackal;
 import static com.miklesam.dota_manager.PlayersInit.KingR;
 import static com.miklesam.dota_manager.PlayersInit.Kodos;
+import static com.miklesam.dota_manager.PlayersInit.LighTofHeaveN;
 import static com.miklesam.dota_manager.PlayersInit.Lil;
 import static com.miklesam.dota_manager.PlayersInit.Maden;
 import static com.miklesam.dota_manager.PlayersInit.Mag;
 import static com.miklesam.dota_manager.PlayersInit.Magical;
+import static com.miklesam.dota_manager.PlayersInit.MeTTpuM;
 import static com.miklesam.dota_manager.PlayersInit.NS;
+import static com.miklesam.dota_manager.PlayersInit.Naive;
 import static com.miklesam.dota_manager.PlayersInit.NoFear;
 import static com.miklesam.dota_manager.PlayersInit.NoOne;
+import static com.miklesam.dota_manager.PlayersInit.PGG;
 import static com.miklesam.dota_manager.PlayersInit.Palantimos;
 import static com.miklesam.dota_manager.PlayersInit.Pikachu;
 import static com.miklesam.dota_manager.PlayersInit.Ramzes;
@@ -38,18 +48,25 @@ import static com.miklesam.dota_manager.PlayersInit.Resolut1on;
 import static com.miklesam.dota_manager.PlayersInit.Rodjer;
 import static com.miklesam.dota_manager.PlayersInit.Santa;
 import static com.miklesam.dota_manager.PlayersInit.Sedoy;
+import static com.miklesam.dota_manager.PlayersInit.Shachlo;
 import static com.miklesam.dota_manager.PlayersInit.Silent;
 import static com.miklesam.dota_manager.PlayersInit.SoNNeikO;
 import static com.miklesam.dota_manager.PlayersInit.Solo;
+import static com.miklesam.dota_manager.PlayersInit.SoloYoung;
 import static com.miklesam.dota_manager.PlayersInit.Vanskor;
+import static com.miklesam.dota_manager.PlayersInit.Vigoss;
+import static com.miklesam.dota_manager.PlayersInit.XSvamp1Re;
 import static com.miklesam.dota_manager.PlayersInit.Zayac;
+import static com.miklesam.dota_manager.PlayersInit.blowyourbrain;
 import static com.miklesam.dota_manager.PlayersInit.bzz;
 import static com.miklesam.dota_manager.PlayersInit.chshrct;
 import static com.miklesam.dota_manager.PlayersInit.dream;
 import static com.miklesam.dota_manager.PlayersInit.illidan;
 import static com.miklesam.dota_manager.PlayersInit.nongrata;
 import static com.miklesam.dota_manager.PlayersInit.p9asha;
+import static com.miklesam.dota_manager.PlayersInit.sQreen;
 import static com.miklesam.dota_manager.PlayersInit.sayuw;
+import static com.miklesam.dota_manager.PlayersInit.tmw;
 import static com.miklesam.dota_manager.PlayersInit.velheor;
 import static com.miklesam.dota_manager.PlayersInit.wejustzik;
 import static com.miklesam.dota_manager.PlayersInit.yoky;
@@ -70,6 +87,15 @@ public class TeamsInit {
     static ArrayList<Players> ferzeeTeam=new ArrayList<Players>();
     static ArrayList<Players> iCCupTeam=new ArrayList<Players>();
     static ArrayList<Players> MoscowFiveTeam=new ArrayList<Players>();
+    static ArrayList<Players> RebelsTeam=new ArrayList<Players>();
+    static ArrayList<Players> RoxKisTeam=new ArrayList<Players>();
+    static ArrayList<Players> TheRetryTeam=new ArrayList<Players>();
+    static ArrayList<Players> DTSTeam=new ArrayList<Players>();
+    static ArrayList<Players> DuzaGamingTeam=new ArrayList<Players>();
+    static ArrayList<Players> NoPangoTeam=new ArrayList<Players>();
+
+
+
 
 
     public static ArrayList VpInit(){
@@ -189,8 +215,62 @@ public class TeamsInit {
         return MoscowFiveTeam;
     }
 
+    public static ArrayList RebelsInit(){
+        RebelsTeam.add(Sedoy);
+        RebelsTeam.add(tmw);
+        RebelsTeam.add(Shachlo);
+        RebelsTeam.add(Vanskor);
+        RebelsTeam.add(MeTTpuM);
+        return RebelsTeam;
+    }
+
+    public static ArrayList RoxKisInit(){
+        RoxKisTeam.add(bzz);
+        RoxKisTeam.add(SoloYoung);
+        RoxKisTeam.add(Sedoy);
+        RoxKisTeam.add(yol);
+        RoxKisTeam.add(Goblak);
+        return RoxKisTeam;
+    }
+
+    public static ArrayList TheRetryInit(){
+        TheRetryTeam.add(PGG);
+        TheRetryTeam.add(BloodAngel);
+        TheRetryTeam.add(blowyourbrain);
+        TheRetryTeam.add(ALWAYSWANNAFLY);
+        TheRetryTeam.add(Goblak);
+        return TheRetryTeam;
+    }
 
 
+    public static ArrayList DTSInit(){
+        DTSTeam.add(Artstyle);
+        DTSTeam.add(Dendi);
+        DTSTeam.add(LighTofHeaveN);
+        DTSTeam.add(Dread);
+        DTSTeam.add(NS);
+        return DTSTeam;
+    }
+
+
+    public static ArrayList DuzaGamingInit(){
+        DuzaGamingTeam.add(bzz);
+        DuzaGamingTeam.add(Vigoss);
+        DuzaGamingTeam.add(GeneRaL);
+        DuzaGamingTeam.add(sQreen);
+        DuzaGamingTeam.add(ArsArt);
+        return DuzaGamingTeam;
+    }
+
+
+    public static ArrayList NoPangoInit(){
+        NoPangoTeam.add(Naive);
+        NoPangoTeam.add(Ceyler);
+        NoPangoTeam.add(Ghostik);
+        NoPangoTeam.add(XSvamp1Re);
+        NoPangoTeam.add(CemaTheSlayer);
+        return NoPangoTeam;
+    }
 
 
 
@@ -198,17 +278,23 @@ public class TeamsInit {
 
     static final Teams VirtusPro = new Teams("Virtus Pro",VPTeam,0,R.drawable.vp_min_logo);
     static final Teams Navi = new Teams("NAVI",NaviTeam,1,R.drawable.navi_min_logo);
-    static final Teams Gambit = new Teams("Gambit",GambitTeam,2,R.drawable.vp_min_logo);
-    static final Teams TeamSpirit = new Teams("Team Spirit",TeamSpiritTeam,3,R.drawable.vp_min_logo);
-    static final Teams Empire = new Teams("Empire",EmpireTeam,4,R.drawable.vp_min_logo);
-    static final Teams Winstrike = new Teams("Winstrike",WinstrikeTeam,5,R.drawable.vp_min_logo);
-    static final Teams BlackHornetsGaming = new Teams(" Black Hornets Gaming",BlackHornetsGamingTeam,6,R.drawable.vp_min_logo);
-    static final Teams FantasticFive = new Teams("FantasticFive",FantasticFiveTeam,7,R.drawable.vp_min_logo);
-    static final Teams CISRejects = new Teams("CISRejects",CISRejectsTeam,8,R.drawable.vp_min_logo);
-    static final Teams FRIENDS = new Teams("F.R.I.E.N.D.S.",FRIENDSTeam,9,R.drawable.vp_min_logo);
-    static final Teams ferzee = new Teams("ferzee.",ferzeeTeam,10,R.drawable.vp_min_logo);
-    static final Teams iCCup = new Teams("iCCup.",iCCupTeam,11,R.drawable.vp_min_logo);
-    static final Teams MoscowFive = new Teams("MoscowFive.",MoscowFiveTeam,12,R.drawable.vp_min_logo);
+    static final Teams Gambit = new Teams("Gambit",GambitTeam,2,R.drawable.gambit_esports_min_logo);
+    static final Teams TeamSpirit = new Teams("Team Spirit",TeamSpiritTeam,3,R.drawable.old_but_gold_logo);
+    static final Teams Empire = new Teams("Empire",EmpireTeam,4,R.drawable.team_empire_min_logo);
+    static final Teams Winstrike = new Teams("Winstrike",WinstrikeTeam,5,R.drawable.winstrike_team_min_logo);
+    static final Teams BlackHornetsGaming = new Teams(" Black Hornets Gaming",BlackHornetsGamingTeam,6,R.drawable.black_hornets_gaming_logo);
+    static final Teams FantasticFive = new Teams("FantasticFive",FantasticFiveTeam,7,R.drawable.fantasticfive);
+    static final Teams CISRejects = new Teams("CISRejects",CISRejectsTeam,8,R.drawable.cisrejects);
+    static final Teams FRIENDS = new Teams("F.R.I.E.N.D.S.",FRIENDSTeam,9,R.drawable.friends_dota);
+    static final Teams ferzee = new Teams("ferzee",ferzeeTeam,10,R.drawable.ferzee_logo);
+    static final Teams iCCup = new Teams("iCCup",iCCupTeam,11,R.drawable.ccup_logo);
+    static final Teams MoscowFive = new Teams("MoscowFive",MoscowFiveTeam,12,R.drawable.m5_logo);
+    static final Teams Rebels = new Teams("Rebels",RebelsTeam,13,R.drawable.rebels_dota);
+    static final Teams RoxKis = new Teams("RoxKis",RoxKisTeam,14,R.drawable.rox_logo);
+    static final Teams TheRetry = new Teams("TheRetry",TheRetryTeam,15,R.drawable.theretry);
+    static final Teams DTS = new Teams("DTS",DTSTeam,16,R.drawable.dts);
+    static final Teams DuzaGaming = new Teams("DuzaGaming",DuzaGamingTeam,17,R.drawable.duzalogo);
+    static final Teams NoPango = new Teams("NoPango",NoPangoTeam,18,R.drawable.nopangolier);
 
 
 
@@ -226,6 +312,14 @@ public class TeamsInit {
         ferzeeInit();
         iCCupInit();
         MoscowFiveInit();
+        RebelsInit();
+        RoxKisInit();
+        TheRetryInit();
+        DTSInit();
+        DuzaGamingInit();
+        NoPangoInit();
+
+
 
         AllTeams.add(VirtusPro);
         AllTeams.add(Navi);
@@ -240,6 +334,12 @@ public class TeamsInit {
         AllTeams.add(ferzee);
         AllTeams.add(iCCup);
         AllTeams.add(MoscowFive);
+        AllTeams.add(Rebels);
+        AllTeams.add(RoxKis);
+        AllTeams.add(TheRetry);
+        AllTeams.add(DTS);
+        AllTeams.add(DuzaGaming);
+        AllTeams.add(NoPango);
 
         return AllTeams;
     }
--- app/src/main/java/com/miklesam/dota_manager/TeamsShow.java
+++ app/src/main/java/com/miklesam/dota_manager/TeamsShow.java
@@ -4,29 +4,99 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import static com.miklesam.dota_manager.TeamsInit.AllTeams;
 import static com.miklesam.dota_manager.TeamsInit.AllTeamsInit;
 
 public class TeamsShow extends AppCompatActivity {
     ListView TeamslistView;
     ArrayList<Teams> ThisTeams;
+    LinearLayout TeamInfos;
+    ListView teamroosts;
+    Button Backinfo;
+    ImageView FlagImage;
+    ImageView signature1;
+    ImageView signature2;
+    ImageView signature3;
+    ArrayList<Heroes> Heroeses= new ArrayList<Heroes>();
+    int teampos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teams_show);
-
+        TeamInfos=findViewById(R.id.TeamInfo);
         TeamslistView = (ListView)findViewById(R.id.TeamList);
+        teamroosts= (ListView)findViewById(R.id.teamroost);
         ThisTeams=AllTeamsInit();
         final TeamsAdapter teamsAdapter=new TeamsAdapter();
+        Backinfo=findViewById(R.id.Backs);
+        FlagImage=findViewById(R.id.FlagImage);
 
+        signature1=findViewById(R.id.signature1);
+        signature2=findViewById(R.id.signature2);
+        signature3=findViewById(R.id.signature3);
 
         TeamslistView.setAdapter(teamsAdapter);
+
+        Heroeses=HeroInit.HeroInit();
+
+        Backinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                teamroosts.setVisibility(View.INVISIBLE);
+                TeamInfos.setVisibility(View.INVISIBLE);
+                TeamslistView.setVisibility(View.VISIBLE);
+                Backinfo.setVisibility(View.INVISIBLE);
+
+            }
+            });
+
+
+        TeamslistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TeamslistView.setVisibility(View.INVISIBLE);
+                TeamInfos.setVisibility(View.VISIBLE);
+                teampos=position;
+                final TeamsInfodapter teamsener=new TeamsInfodapter();
+                teamroosts.setAdapter(teamsener);
+                teamroosts.setVisibility(View.VISIBLE);
+                Backinfo.setVisibility(View.VISIBLE);
+
+
+
+
+            }
+        });
+
+
+
+        teamroosts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                FlagImage.setImageResource(AllTeams.get(teampos).Players.get(position).Flag);
+
+                signature1.setImageResource(Heroeses.get(AllTeams.get(teampos).Players.get(position).signature1).picked);
+                signature2.setImageResource(Heroeses.get(AllTeams.get(teampos).Players.get(position).signature2).picked);
+                signature3.setImageResource(Heroeses.get(AllTeams.get(teampos).Players.get(position).signature3).picked);
+
+
+            }
+        });
+
+
+
 
 
     }
@@ -75,6 +145,58 @@ public class TeamsShow extends AppCompatActivity {
             return convertView;
         }
     }
+
+
+
+
+
+    class TeamsInfodapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            convertView = getLayoutInflater().inflate(R.layout.teaminf,null);
+
+            ImageView imageView=(ImageView)convertView.findViewById(R.id.image);
+            TextView textView = (TextView)convertView.findViewById(R.id.Titile);
+
+
+            //imageView.setImageResource(CoreIMAGES[position]);
+            //textView.setText(CoreNAMES[position]);
+            //Price.setText(CorePriceMoney[position]);
+
+
+            //imageView.setImageResource(ThisTeams.get(position).Flag);
+
+            textView.setText(ThisTeams.get(teampos).Players.get(position).Name);
+            imageView.setImageResource(ThisTeams.get(teampos).Players.get(position).Flag);
+            //Price.setText(String.valueOf(Cores.get(position).Cost));
+
+
+
+
+
+
+            return convertView;
+        }
+    }
+
+
 
 
 }
