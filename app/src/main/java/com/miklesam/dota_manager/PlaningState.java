24dc4681a02c505c11c6f3fcde28eea0cbee9524
Miklesami <mikle.samarkin@gmail.com>
2019-07-05 10:24:17 +0300

    planeingstate

--- app/src/main/java/com/miklesam/dota_manager/PlaningState.java
+++ app/src/main/java/com/miklesam/dota_manager/PlaningState.java
@@ -37,9 +37,7 @@ public class PlaningState extends AppCompatActivity {
     boolean midlline[]= new boolean[5];
     boolean bottomlline[]= new boolean[5];
 
-    int bothero[]=new int[5];
-    int midhero[]=new int[5];
-    int tophero[]=new int[5];
+
     Button ToFightStage;
     int lane_id[]= new int[5];
 
@@ -52,6 +50,10 @@ public class PlaningState extends AppCompatActivity {
     ArrayList<Integer> CompHeroesAndLanes = new ArrayList<Integer>();
     ArrayList<Planers> TeamPlaners = new ArrayList<Planers>();
     ArrayList<Teams> AllTeamsTeams = new ArrayList<Teams>();
+
+    ArrayList top = new ArrayList<>();
+    ArrayList mid = new ArrayList<>();
+    ArrayList bot = new ArrayList<>();
 
     int TeamEnemy;
 
@@ -256,6 +258,22 @@ public class PlaningState extends AppCompatActivity {
 
 
 
+       top.add(Hero[0]);
+       mid.add(Hero[1]);
+       bot.add(Hero[2]);
+       bot.add(Hero[3]);
+       bot.add(Hero[4]);
+
+        for (int i = 0; i < top.size(); i++) {
+            Top[i].setVisibility(View.VISIBLE);
+        }
+        for (int i = 0; i < mid.size(); i++) {
+            Mid[i].setVisibility(View.VISIBLE);
+        }
+        for (int i = 0; i < bot.size(); i++) {
+            Bottom[i].setVisibility(View.VISIBLE);
+        }
+
 
 
 
@@ -271,188 +289,65 @@ for(int k=0;k<5;k++)
         public void onItemSelected(AdapterView<?> parent, View view,
                                    int position, long id) {
 
+            for(int i=0;i<top.size();i++)
+            {
+                if((Integer) top.get(i)==Hero[finalK])
+                {
+                    top.remove(i);
+                }
+                if((Integer) mid.get(i)==Hero[finalK])
+                {
+                    mid.remove(i);
+                }
+                if((Integer) bot.get(i)==Hero[finalK])
+                {
+                    bot.remove(i);
+                }
+            }
 
-            // показываем позиция нажатого элемента
-            //Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+
             if (position==0)
             {
-                lane_id[finalK]=1;
-
-                //Top[0].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                // Top[0].setVisibility(View.VISIBLE);
-                for(int i=0;i<5;i++)
-                {
-                    if(midhero[i]==Hero[finalK])
-                    {
-                        Mid[i].setVisibility(View.INVISIBLE);
-                        midlline[i]=false;
-                    }
-                    else if(bothero[i]==Hero[finalK])
-                    {
-                        Bottom[i].setVisibility(View.INVISIBLE);
-                        bottomlline[i]=false;
-                    }
-
-                }
-
-                if(toplline[0]==false)
-                {
-                    Top[0].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Top[0].setVisibility(View.VISIBLE);
-                    toplline[0]=true;
-                    tophero[0]=Hero[finalK];
-                }
-                else if(toplline[1]==false)
-                {
-                    Top[1].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Top[1].setVisibility(View.VISIBLE);
-                    toplline[1]=true;
-                    tophero[1]=Hero[finalK];
-                }
-                else if(toplline[2]==false)
-                {
-                    Top[2].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Top[2].setVisibility(View.VISIBLE);
-                    toplline[2]=true;
-                    tophero[2]=Hero[finalK];
-                }
-                else if(toplline[3]==false)
-                {
-                    Top[3].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Top[3].setVisibility(View.VISIBLE);
-                    toplline[3]=true;
-                    tophero[3]=Hero[finalK];
-                }
-
-                else if(toplline[4]==false)
-                {
-                    Top[4].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Top[4].setVisibility(View.VISIBLE);
-                    toplline[4]=true;
-                    tophero[4]=Hero[finalK];
-                }
-
-
-
-
-
-
-
-
+                top.add(Hero[finalK]);
             }
             else if (position==1)
             {
-                lane_id[finalK]=2;
-                for(int i=0;i<5;i++)
-                {
-                    if(bothero[i]==Hero[finalK])
-                    {
-                        Bottom[i].setVisibility(View.INVISIBLE);
-                        bottomlline[i]=false;
-                    }
-                    else if(tophero[i]==Hero[finalK])
-                    {
-                        Top[i].setVisibility(View.INVISIBLE);
-                        toplline[i]=false;
-                    }
-
-                }
-
-                if(midlline[0]==false)
-                {
-                    Mid[0].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Mid[0].setVisibility(View.VISIBLE);
-                    midlline[0]=true;
-                    midhero[0]=Hero[finalK];
-                }
-                else if(midlline[1]==false)
-                {
-                    Mid[1].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Mid[1].setVisibility(View.VISIBLE);
-                    midlline[1]=true;
-                    midhero[1]=Hero[finalK];
-                }
-                else if(midlline[2]==false)
-                {
-                    Mid[2].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Mid[2].setVisibility(View.VISIBLE);
-                    midlline[2]=true;
-                    midhero[2]=Hero[finalK];
-                }
-                else if(midlline[3]==false)
-                {
-                    Mid[3].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Mid[3].setVisibility(View.VISIBLE);
-                    midlline[3]=true;
-                    midhero[3]=Hero[finalK];
-                }
-
-                else if(midlline[4]==false)
-                {
-                    Mid[4].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Mid[4].setVisibility(View.VISIBLE);
-                    midlline[4]=true;
-                    midhero[4]=Hero[finalK];
-                }
-
-
+                mid.add(Hero[finalK]);
             }
             else if (position==2)
             {
-                lane_id[finalK]=3;
-                for(int i=0;i<5;i++)
-                {
-                    if(midhero[i]==Hero[finalK])
-                    {
-                        Mid[i].setVisibility(View.INVISIBLE);
-                        midlline[i]=false;
-                    }
-
-                    else if(tophero[i]==Hero[finalK])
-                    {
-                        Top[i].setVisibility(View.INVISIBLE);
-                        toplline[i]=false;
-                    }
-
-                }
-
-                if(bottomlline[0]==false)
-                {
-                    Bottom[0].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Bottom[0].setVisibility(View.VISIBLE);
-                    bottomlline[0]=true;
-                    bothero[0]=Hero[finalK];
-                }
-                else if(bottomlline[1]==false)
-                {
-                    Bottom[1].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Bottom[1].setVisibility(View.VISIBLE);
-                    bottomlline[1]=true;
-                    bothero[1]=Hero[finalK];
-                }
-                else if(bottomlline[2]==false)
-                {
-                    Bottom[2].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Bottom[2].setVisibility(View.VISIBLE);
-                    bottomlline[2]=true;
-                    bothero[2]=Hero[finalK];
-                }
-                else if(bottomlline[3]==false)
-                {
-                    Bottom[3].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Bottom[3].setVisibility(View.VISIBLE);
-                    bottomlline[3]=true;
-                    bothero[3]=Hero[finalK];
-                }
-
-                else if(bottomlline[4]==false)
-                {
-                    Bottom[4].setImageResource(AllHeroes.get(Hero[finalK]).mipmap);
-                    Bottom[4].setVisibility(View.VISIBLE);
-                    bottomlline[4]=true;
-                    bothero[4]=Hero[finalK];
-                }
+                bot.add(Hero[finalK]);
             }
+
+
+
+
+
+            for (int i=0;i<top.size();i++){
+                Top[i].setImageResource(AllHeroes.get((Integer) top.get(i)).mipmap);
+            }
+            for (int i=0;i<mid.size();i++){
+                Mid[i].setImageResource(AllHeroes.get((Integer) mid.get(i)).mipmap);
+            }
+            for (int i=0;i<top.size();i++){
+                Bottom[i].setImageResource(AllHeroes.get((Integer) bot.get(i)).mipmap);
+            }
+
+
+            for (int i = 0; i < top.size(); i++) {
+                Top[i].setVisibility(View.VISIBLE);
+            }
+            for (int i = 0; i < mid.size(); i++) {
+                Mid[i].setVisibility(View.VISIBLE);
+            }
+            for (int i = 0; i < bot.size(); i++) {
+                Bottom[i].setVisibility(View.VISIBLE);
+            }
+
+
+
+
+
 
 
 
